64e9c1ea1d7ca82050031feb0520b87ecb5613fc
Gian Carlos Liriano Adames <giancarlos29@hotmail.com>
2019-02-23 09:08:29 -0400

    Second Commit, done with Delete

--- Proyecto77/App_Start/RouteConfig.cs
+++ Proyecto77/App_Start/RouteConfig.cs
@@ -14,11 +14,6 @@ namespace Proyecto77
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Delete",
-                "Delete/{id}",
-                new { controller = "Home", action = "Index" });
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
